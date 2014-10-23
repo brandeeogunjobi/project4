faf86d64d857165ebb8928318d4be6c89f424731
brandeeogunjobi <ketaloginc@brandees-MacBook-Pro.local>
2014-10-23 12:48:01 -0700

    Changes to my repo

---
+++ Ogunjobi_Brandee_Project4.js
@@ -0,0 +1,75 @@
+//alert("JavaScript works!");
+
+//-----------String Library-----------//
+
+	//Does a string follow a 123-456-7890 pattern like a phone number?
+	     var phoneValidate = function(phone) {
+ var phonesec = phone.split('-'); // ['123', '456', '7890']
+ if(phonesec.length === 3) {
+  if(!isNaN(phonesec[0]) && !isNaN(phonesec[1]) && !isNaN(phonesec[2])) {
+  if(phonesec[0].length === 3 && phonesec[1].length === 3 && phonesec[2].length === 4) {
+  return true;
+  }
+  }
+ }
+
+ return false;
+};
+
+var emailValidate = function(email) {
+ var emailsec = email.split('@'); // ['somename', 'domain.tld']
+ if(emailsec.length === 2) { //Check that there is a single @ symbol with strings on each side
+  if(emailsec[0].length > 0) { //Check that the username side is at least 1 character in length
+  domainsec = emailsec[1].split('.'); //Split the domain on .
+  if(domainsec.length > 1) { //Check that there are at least 2 sections in the domain (domain and tld at a minimum)
+  if(domainsec[domainsec.length - 1].length > 1) { //Check length of final section (tld), and make sure it is at least 2 characters
+  for(i = 0; i < domainsec.length; i++) { //Check each piece of the domain and make sure there is at least one character in it
+  if(domainsec[i].length < 1)
+  return false; //Return false if any section fails
+  }
+  return true;
+  }
+  }
+  }
+ }
+
+ return false
+};
+
+console.log(emailValidate('superlinkx@gmail.com'));
+console.log(emailValidate('@gmail.com'));
+console.log(emailValidate('hello@gmail'));
+console.log(emailValidate('hello.gmail.com'));
+console.log(emailValidate('hello@www.gmail.com'));
+
+
+	//Does a string follow an aaa@bbb.ccc pattern like an email address?
+	
+	
+    
+    //Is the string a URL? (Does it start with http: or https:?)
+    
+    //Title-case a string (split into words, then uppercase the first letter of each 			      word)
+    
+    /*Given a string that is a list of things separated by a given string, as well as 									      another string separator,
+      return a string with the first separator changed to the second: "a,b,c" + "," +      "/"    ? "a/b/c".*/
+
+//-----------Number Library-----------//
+    //Format a number to use a specific number of decimal places, as for money: 2.1 ? 	      2.10
+    
+    //Fuzzy-match a number: is the number above or below a number within a certain      percent?
+    
+    //Find the number of hours or days difference between two dates.
+    
+    //Given a string version of a number such as "42", return the value as an actual      Number, such as 42.
+    
+//-----------Array Library-----------//
+    //Find the smallest value in an array that is greater than a given number
+
+    
+  
+    //Find the total value of just the numbers in an array, even if some of the items 	      are not numbers.
+    
+    /*Given an array of objects and the name of a key, return the array sorted by the      value of that key in each of the objects: "a"
+      + [{a:2},{a:3},{a:1}] ? [{a:1},{a:2},{a:3}].*/
+ 
